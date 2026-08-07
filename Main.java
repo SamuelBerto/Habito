c010da39b1ac2b5a31a88a1a248588303e5b27bd
Samuel Berto <samuztberto@gmail.com>
2026-08-06 22:54:51 -0300

    Permite cadastrar múltiplos hábitos em loop até digitar 'sair'

--- Main.java
+++ Main.java
@@ -7,16 +7,26 @@ public class Main {
         ArrayList<Habito> habitos = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite o nome do hábito:");
-        String nome = scanner.nextLine();
+        while (true) {
+            System.out.println("Digite o nome do hábito (ou 'sair' para encerrar):");
+            String nome = scanner.nextLine();
+            
+            if (nome.equalsIgnoreCase("sair")) {
+                break;
+            }
+            System.out.println("Digite um emoji para representar esse hábito:");
+            String emoji = scanner.nextLine();
+            
+            Habito novoHabito = new Habito(nome, emoji);
+            habitos.add(novoHabito);
+            
+            System.out.println("Hábito cadastrado: " + novoHabito.getEmoji() + " " + novoHabito.getNome());    
+        }
 
-        System.out.println("Digite o emoji para representar o hábito:");
-        String emoji = scanner.nextLine();
-
-        Habito novoHabito = new Habito(nome, emoji);
-        habitos.add(novoHabito);
-
-        System.out.println("Hábito Cadastrado: " + novoHabito.getEmoji() + " " + novoHabito.getNome());
+        System.out.println("\n=== Hábitos cadastrados ===");
+        for (Habito h : habitos) {
+            System.out.println(h.getEmoji() + " " + h.getNome());
+        }
 
         scanner.close();
        
